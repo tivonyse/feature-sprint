ae5c39c49ad7c5e0017e0a6aead005b51628ee30
Tivon Yuen <tivonyuen@keyteo.com>
2023-02-28 15:55:41 +0800

    initial commit

---
+++ src/main/java/tylab/io/featuresprint/FeatureSprintApplication.java
@@ -0,0 +1,13 @@
+package tylab.io.featuresprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureSprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tylab/io/featuresprint/FeatureSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package tylab.io.featuresprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
